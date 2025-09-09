27168e5198d4ad23533e45a8bf4c6312dc203f54
Yash <thotayash47@gmail.com>
2025-09-09 12:19:43 +0530

    ui made betteer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Habit Tracker';
+  constructor(private router: Router) {}
+  isAuthRoute(): boolean {
+    const route = this.router.url.split('?')[0].split('#')[0];
+    return route.startsWith('/login') || route.startsWith('/signup');
+  }
 }
